e8d2a52ab3def62b091d186a511ae2e6043b244d
Tanvir0922 <tanvirshaikh7116@gmail.com>
2025-08-08 16:47:01 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -7,7 +7,8 @@ define('DB_NAME','tms');
 // Establish database connection.
 try
 {
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+$db = new PDO('mysql:host=mysql;dbname=tmsdb', 'tmsuser', 'tmspass');
+
 }
 catch (PDOException $e)
 {
